fbad368e4243a5359cd813686e2fc72bf8741855
Pamela Fox <pamela.fox@gmail.com>
2023-01-25 00:14:42 +0000

    Black formatting

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
 from datetime import datetime
 import os
 
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+)
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from requests import RequestException
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder="static")
 csrf = CSRFProtect(app)
 
 # If RUNNING_IN_PRODUCTION is defined as an environment variable, then we're running on Azure
-if not 'RUNNING_IN_PRODUCTION' in os.environ:
+if not "RUNNING_IN_PRODUCTION" in os.environ:
     # Local development, where we'll use environment variables.
     print("Loading config.development and environment variables from .env file.")
-    app.config.from_object('settings.development')
+    app.config.from_object("settings.development")
 else:
     # Production, we don't load environment variables from .env file but add them as environment variables in Azure.
     print("Loading config.production.")
-    app.config.from_object('settings.production')
+    app.config.from_object("settings.production")
 
 with app.app_context():
     app.config.update(
-        SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+        SQLALCHEMY_DATABASE_URI=app.config.get("DATABASE_URI"),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
     )
 
@@ -36,61 +43,76 @@ migrate = Migrate(app, db)
 from models import Survey, Answer
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    return redirect(url_for('surveys_list_page'))
+    return redirect(url_for("surveys_list_page"))
 
-@app.route('/surveys', methods=['GET'])
+
+@app.route("/surveys", methods=["GET"])
 def surveys_list_page():
-    surveys = Survey.query.all()    
-    return render_template('surveys_list.html', surveys=surveys)
+    surveys = Survey.query.all()
+    return render_template("surveys_list.html", surveys=surveys)
 
-@app.route('/surveys/new', methods=['GET'])
+
+@app.route("/surveys/new", methods=["GET"])
 def surveys_create_page():
-    return render_template('surveys_create.html')
+    return render_template("surveys_create.html")
 
-@app.route('/surveys', methods=['POST'])
+
+@app.route("/surveys", methods=["POST"])
 @csrf.exempt
 def surveys_create_handler():
-    question = request.values.get('survey_question')
-    topic = request.values.get('survey_topic')
-    options = request.values.get('survey_options')
+    question = request.values.get("survey_question")
+    topic = request.values.get("survey_topic")
+    options = request.values.get("survey_options")
     survey = Survey()
     survey.topic = topic
     survey.question = question
     survey.options = options
     db.session.add(survey)
     db.session.commit()
-    return redirect(url_for('survey_page', survey_id=survey.id))
+    return redirect(url_for("survey_page", survey_id=survey.id))
 
-@app.route('/surveys/<int:survey_id>', methods=['GET'])
+
+@app.route("/surveys/<int:survey_id>", methods=["GET"])
 def survey_page(survey_id):
     survey = Survey.query.where(Survey.id == survey_id).first()
-    answers = Survey.query.where(Answer.survey==survey_id)
-    return render_template('survey_details.html', survey=survey, answers=answers, already_voted='survey_id' in request.cookies)
+    answers = Survey.query.where(Answer.survey == survey_id)
+    return render_template(
+        "survey_details.html",
+        survey=survey,
+        answers=answers,
+        already_voted="survey_id" in request.cookies,
+    )
 
-@app.route('/surveys/<int:survey_id>/answers', methods=['POST'])
+
+@app.route("/surveys/<int:survey_id>/answers", methods=["POST"])
 @csrf.exempt
 def answers_create_handler(survey_id):
     # Check cookie to prevent multiple votes
-    if 'survey_id' in request.cookies:
-        return redirect(url_for('survey_page', survey_id=survey_id))
+    if "survey_id" in request.cookies:
+        return redirect(url_for("survey_page", survey_id=survey_id))
     # Store their answer in database
-    option = request.values.get('option')
+    option = request.values.get("option")
     answer = Answer()
     answer.survey = survey_id
     answer.selected_option = option.strip()
     db.session.add(answer)
     db.session.commit()
-    resp = redirect(url_for('survey_page', survey_id=survey_id))   
+    resp = redirect(url_for("survey_page", survey_id=survey_id))
     # Set cookie on the response
-    resp.set_cookie('survey_id', str(survey_id))
+    resp.set_cookie("survey_id", str(survey_id))
     return resp
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-if __name__ == '__main__':
-   app.run()
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
+
+if __name__ == "__main__":
+    app.run()
--- models.py
+++ models.py
@@ -5,16 +5,17 @@ from sqlalchemy.orm import declarative_base, validates
 
 from app import db
 
+
 class Survey(db.Model):
-    __tablename__ = 'survey'
+    __tablename__ = "survey"
     id = Column(Integer, primary_key=True)
     topic = Column(String(50))
     question = Column(String(150))
-    options = Column(String(500)) # New-line separated options
+    options = Column(String(500))  # New-line separated options
 
     @property
     def options_list(self):
-        return [option.strip() for option in self.options.split('\n')]
+        return [option.strip() for option in self.options.split("\n")]
 
     @property
     def answer_count(self):
@@ -24,17 +25,25 @@ class Survey(db.Model):
     def option_stats(self):
         """Returns a dictionary of option counts"""
         answer_count = self.answer_count
-        option_count = {option: {"count": 0, "percent": 0} for option in self.options_list}
+        option_count = {
+            option: {"count": 0, "percent": 0} for option in self.options_list
+        }
         for answer in Answer.query.filter_by(survey=self.id):
             if answer.selected_option in option_count:
                 option_count[answer.selected_option]["count"] += 1
-                option_count[answer.selected_option]["percent"] = round(option_count[answer.selected_option]["count"] / answer_count * 100, 2)
+                option_count[answer.selected_option]["percent"] = round(
+                    option_count[answer.selected_option]["count"] / answer_count * 100,
+                    2,
+                )
             else:
-                logging.warning('No matching option found for [%s]', answer.selected_option)
+                logging.warning(
+                    "No matching option found for [%s]", answer.selected_option
+                )
         return option_count
-        
+
+
 class Answer(db.Model):
-    __tablename__ = 'answer'
+    __tablename__ = "answer"
     id = Column(Integer, primary_key=True)
-    survey = Column(Integer, ForeignKey('survey.id', ondelete="CASCADE"))
+    survey = Column(Integer, ForeignKey("survey.id", ondelete="CASCADE"))
     selected_option = Column(String(500))
