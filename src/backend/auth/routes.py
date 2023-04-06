829c7dca9a82dc9bceb43269061a4accf04b29b0
Pamela Fox <pamela.fox@gmail.com>
2023-04-06 11:12:42 -0700

    Store next_url in session instead of state, to leave state for other uses

--- src/backend/auth/routes.py
+++ src/backend/auth/routes.py
@@ -1,5 +1,5 @@
 import identity
-from flask import current_app, redirect, render_template, request, url_for
+from flask import current_app, redirect, render_template, request, session, url_for
 
 from backend.auth import bp
 
@@ -7,14 +7,11 @@ from backend.auth import bp
 @bp.route("/login")
 def login():
     auth = current_app.config["AUTH"]
+    session["next_url"] = request.args.get("next_url", url_for("index"))
     return render_template(
         "auth/login.html",
         version=identity.__version__,
-        **auth.log_in(
-            scopes=current_app.config["SCOPE"],  # Have user consent to scopes during log-in
-            redirect_uri=url_for(".auth_response", _external=True),
-            state=request.args.get("next_url", url_for("index")),
-        ),
+        **auth.log_in(scopes=current_app.config["SCOPE"], redirect_uri=url_for(".auth_response", _external=True)),
     )
 
 
@@ -24,7 +21,8 @@ def auth_response():
     result = auth.complete_log_in(request.args)
     if "error" in result:
         return render_template("auth/auth_error.html", result=result)
-    return redirect(request.args.get("state", url_for("index")))
+    next_url = session.pop("next_url", url_for("index"))
+    return redirect(next_url)
 
 
 @bp.route("/logout")
